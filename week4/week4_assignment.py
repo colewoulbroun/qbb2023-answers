b71316493e9c9b80f1dbd8bc7de4aa84358ffea1
Cole <cwoulbr1@jh.edu>
2023-11-20 16:57:00 -0500

    Submitting a regrade request with updated code for the week 4 assignment.

--- week4/week4_assignment.py
+++ week4/week4_assignment.py
@@ -8,156 +8,167 @@ from scipy.stats import poisson
 
 # Step 1: Calling Peaks
 
-# def main():
+def main():
 
-#     if len(sys.argv) != 6:
-#         print("Usage: python livecodingweek4.py <forward_fname> <reverse_fname> <controlf_fname> <controlr_fname> <out_fname>")
-#         sys.exit(1)
+    if len(sys.argv) != 6:
+        print("Usage: python livecodingweek4.py <forward_fname> <reverse_fname> <controlf_fname> <controlr_fname> <out_fname>")
+        sys.exit(1)
 
-#     forward_fname, reverse_fname, controlf_fname, controlr_fname, out_fname = sys.argv[1:]
+    forward_fname, reverse_fname, controlf_fname, controlr_fname, out_fname = sys.argv[1:]
 
-#     # defining the fragment width here based on estimated size from live coding
-#     fragment_width = 198
+    # defining the fragment width here based on estimated size from live coding
+    fragment_width = 198
 
-#     # specifying the genomic region to analyze
-#     chrom = "chr2R"
-#     chromstart = 10000000
-#     chromend = 12000000
-#     chromlen = chromend - chromstart
+    # specifying the genomic region to analyze
+    chrom = "chr2R"
+    chromstart = 10000000
+    chromend = 12000000
+    chromlen = chromend - chromstart
 
-#     # combining forward and reverse data
-#     combined_sample_bedgraph = combine_bedgraphs(forward_fname, reverse_fname, chrom, chromstart, chromend, 2 * fragment_width)
+    # combining forward and reverse data
+    combined_sample_bedgraph = combine_bedgraphs(forward_fname, reverse_fname, chrom, chromstart, chromend, 2 * fragment_width)
 
-#     combined_control_bedgraph = combine_bedgraphs(controlf_fname, controlr_fname, chrom, chromstart, chromend, 2 * fragment_width)
+    combined_control_bedgraph = combine_bedgraphs(controlf_fname, controlr_fname, chrom, chromstart, chromend, 2 * fragment_width)
 
-#     # comparing to background and calculations
-#     control_background = calculate_background(combined_control_bedgraph, chrom, chromstart, chromend, width=1000)
+    # comparing to background and calculations
+    control_background = calculate_background(combined_control_bedgraph, chrom, chromstart, chromend, width=1000)
     
-#     # scoring samples and generating p-values
-#     sample_scores = score_sample(combined_sample_bedgraph, fragment_width)
+    # scoring samples and generating p-values
+    sample_scores = score_sample(combined_sample_bedgraph, fragment_width)
     
-#     sample_pvalues = calculate_pvalues(sample_scores, control_background)
+    sample_pvalues = calculate_pvalues(sample_scores, control_background)
 
-#     # write wiggle and bed files
-#     write_wiggle(sample_pvalues, chrom, chromstart, out_fname + ".wig")
+    # write wiggle and bed files
+    write_wiggle(sample_pvalues, chrom, chromstart, out_fname + ".wig")
 
-#     write_bed(sample_pvalues, chrom, chromstart, chromend, fragment_width, out_fname + ".bed")
+    write_bed(sample_pvalues, chrom, chromstart, chromend, fragment_width, out_fname + ".bed")
 
-#     significant_peaks = []
+    significant_peaks = []
 
-#     # print the peaks with their chromosomal location
-#     print("Significantly enriched peaks:")
-#     position = chromstart
-#     for index, p_value in enumerate(sample_pvalues):
-#         if -numpy.log10(p_value) >= 10:  # Adjust the threshold as needed
-#             start = position
-#             end = position + fragment_width
-#             significant_peaks.append((start, end, -numpy.log10(p_value)))
-#             print(f"Chromosome: {chrom}, Start: {start}, End: {end}, -log10 P-value: {-numpy.log10(p_value)}")
-#         position = position + 1
+    # print the peaks with their chromosomal location
+    print("Significantly enriched peaks:")
+    position = chromstart
+    for index, p_value in enumerate(sample_pvalues):
+        if -numpy.log10(p_value) >= 10:  # Adjust the threshold as needed
+            start = position
+            end = position + fragment_width
+            significant_peaks.append((start, end, -numpy.log10(p_value)))
+            print(f"Chromosome: {chrom}, Start: {start}, End: {end}, -log10 P-value: {-numpy.log10(p_value)}")
+        position = position + 1
 
-#     # print the number of significantly enriched peaks
-#     significant_peaks_number = len(significant_peaks)
-#     print(f"Number of significantly enriched peaks: {significant_peaks_number}")
-
-
-# # define additional functions
-# def load_bedgraph(fname, target, chromstart, chromend):
-#     # Create array to hold tag counts
-#     coverage = numpy.zeros(chromend - chromstart, int)
-
-#     #Read the file in line by line
-#     for line in open(fname):
-#         # Break the line into individual fields
-#         chrom, start, end, score = line.rstrip().split('\t')
-#         # Check if the data fall in our target region
-#         if chrom != target:
-#             continue
-#         start = int(start)
-#         end = int(end)
-#         if start < chromstart or end >= chromend:
-#             continue
-#         # Add tags to our array
-#         coverage[start-chromstart:end-chromend] = int(score)
-#     return coverage
+    # print the number of significantly enriched peaks
+    significant_peaks_number = len(significant_peaks)
+    print(f"Number of significantly enriched peaks: {significant_peaks_number}")
 
 
-# def combine_bedgraphs(forward_fname, reverse_fname, chrom, chromstart, chromend, shift):
-#     # Load the forward and reverse bedgraph data
-#     forward_bedgraph = load_bedgraph(forward_fname, chrom, chromstart, chromend)
-#     reverse_bedgraph = load_bedgraph(reverse_fname, chrom, chromstart, chromend)
+# define additional functions
+def load_bedgraph(fname, target, chromstart, chromend):
+    # Create array to hold tag counts
+    coverage = numpy.zeros(chromend - chromstart, int)
 
-#     # Combine the forward and reverse data and shift by 'shift'
-#     combined_bedgraph = [f + r for f, r in zip(forward_bedgraph, reverse_bedgraph)]
-#     return combined_bedgraph
-
-
-# def calculate_background(control_bedgraph, chrom, chromstart, chromend, width):
-#     background = []
-
-#     for i in range(chromend - chromstart):
-#         start = max(0, i - width // 2)
-#         end = min(chromend - chromstart, i + width // 2 + 1)
-
-#         # Calculate the mean of control_bedgraph within the window
-#         mean_value = sum(control_bedgraph[start:end]) / (end - start)
-
-#         background.append(mean_value)
-
-#     return background
+    #Read the file in line by line
+    for line in open(fname):
+        # Break the line into individual fields
+        chrom, start, end, score = line.rstrip().split('\t')
+        # Check if the data fall in our target region
+        if chrom != target:
+            continue
+        start = int(start)
+        end = int(end)
+        if start < chromstart or end >= chromend:
+            continue
+        # Add tags to our array
+        coverage[start-chromstart:end-chromend] = int(score)
+    return coverage
 
 
-# def score_sample(sample_bedgraph, fragment_width):
-#     scores = []
+def combine_bedgraphs(forward_fname, reverse_fname, chrom, chromstart, chromend, shift):
+    # Load the forward and reverse bedgraph data
+    forward_bedgraph = load_bedgraph(forward_fname, chrom, chromstart, chromend)
+    reverse_bedgraph = load_bedgraph(reverse_fname, chrom, chromstart, chromend)
 
-#     for i in range(len(sample_bedgraph)):
-#         start = max(0, i - fragment_width)
-#         end = min(len(sample_bedgraph), i + fragment_width + 1)
+    # Combine the forward and reverse data and shift by 'shift'
+    combined_bedgraph = [f + r for f, r in zip(forward_bedgraph, reverse_bedgraph)]
 
-#         # Calculate the score by summing the sample bedgraph values within the window
-#         score = sum(sample_bedgraph[start:end])
+    # Adjust coverage for combined bedgraphs
+    combined_bedgraph = combined_bedgraph[shift:] + [0] * shift
 
-#         scores.append(score)
-
-#     return scores
-
-
-# def calculate_pvalues(sample_scores, control_background):
-#     pvalues = []
-
-#     for score, background_mean in zip(sample_scores, control_background):
-#         p = 1 - poisson.cdf(score, background_mean)
-
-#         pvalues.append(p)
-
-#     return pvalues
+    return combined_bedgraph
 
 
-# def write_wiggle(pvalues, chrom, chromstart, fname):
-#     output = open(fname, 'w')
-#     print(f"fixedStep chrom={chrom} start={chromstart + 1} step=1 span=1", file=output)
-#     for i in pvalues:
-#         print(i, file=output)
-#     output.close()
+def calculate_background(control_bedgraph, chrom, chromstart, chromend, width):
+    background = []
+
+    for i in range(chromend - chromstart):
+        start = max(0, i - width // 2)
+        end = min(chromend - chromstart, i + width // 2 + 1)
+
+        # Calculate the mean of control_bedgraph within the window
+        mean_value = sum(control_bedgraph[start:end]) / (end - start)
+
+        # Multiply the mean by 2 * width bases
+        mean_value = mean_value * (2 * width)
+
+        background.append(mean_value)
+
+    return background
 
 
-# def write_bed(pvalues, chrom, chromstart, chromend, width, fname):
-#     chromlen = chromend - chromstart
-#     enriched_peaks = []
+def score_sample(sample_bedgraph, fragment_width):
+    scores = []
 
-#     for position, p_value in enumerate(pvalues):
-#         if -numpy.log10(p_value) >= 10:  
-#             start = position + chromstart
-#             end = start + width
-#             enriched_peaks.append((chrom, start, end, -numpy.log10(p_value)))
+    for i in range(len(sample_bedgraph)):
+        start = max(0, i - fragment_width)
+        end = min(len(sample_bedgraph), i + fragment_width + 1)
 
-#     with open(fname, 'w') as output:
-#         for peak in enriched_peaks:
-#             print(f"{peak[0]}\t{peak[1]}\t{peak[2]}\t{peak[3]}", file=output)
+        # Calculate the score by summing the sample bedgraph values within the window
+        score = sum(sample_bedgraph[start:end])
+
+        scores.append(score)
+
+    return scores
 
 
-# if __name__ == "__main__":
-#     main()
+def calculate_pvalues(sample_scores, control_background):
+    pvalues = []
+
+    for score, background_mean in zip(sample_scores, control_background):
+        p = 1 - poisson.cdf(score, background_mean)
+
+        pvalues.append(p)
+
+    return pvalues
+
+
+
+def write_wiggle(pvalues, chrom, chromstart, fname):
+    output = open(fname, 'w')
+    print(f"fixedStep chrom={chrom} start={chromstart + 1} step=1 span=1", file=output)
+    for i in pvalues:
+        # Change values equal to 0 to an arbitrarily small number
+        i = min(-numpy.log10(i) if i != 0 else -numpy.log10(1e-10), 50)
+        print(i, file=output)
+    output.close()
+
+
+
+def write_bed(pvalues, chrom, chromstart, chromend, width, fname):
+    chromlen = chromend - chromstart
+    enriched_peaks = []
+
+    for position, p_value in enumerate(pvalues):
+        if -numpy.log10(p_value) >= 10:  
+            start = position + chromstart
+            end = start + width
+            enriched_peaks.append((chrom, start, end, -numpy.log10(p_value)))
+
+    with open(fname, 'w') as output:
+        for peak in enriched_peaks:
+            print(f"{peak[0]}\t{peak[1]}\t{peak[2]}\t{peak[3]}", file=output)
+
+
+if __name__ == "__main__":
+    main()
 
 
 # Step 2: Intersecting Peaks
